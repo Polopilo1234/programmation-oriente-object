88e7d1317a585f3c4865da6d0d4ab8d9f57545b9
Polopilo1234 <160126960+Polopilo1234@users.noreply.github.com>
2024-11-27 14:38:17 +0100

    Add files via upload
    code source du TP1 de POO

---
+++ TPS/Java/TP01/exercice2/HelloWorldApp.java
@@ -0,0 +1,10 @@
+ /**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+ public class HelloWorldApp {
+         public static void main(String[] args) {
+             System.out.println("BONJOUR"); // Display the string.   
+              System.out.println("CECI EST MON PREMIER PROGRAMME JAVA"); // Display the string.   
+         }
+}
